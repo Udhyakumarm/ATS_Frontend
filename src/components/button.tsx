a4f7460f30c254cc4c062fa166d87eacd51eda5c
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-02-28 16:32:59 +0300

    Refactored code

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,20 +1,18 @@
-export default function Button(props: any) {
-    return (
-        <>
-            <button 
-            type={props.btnType ? props.btnType : 'button'} 
-            className={`my-2 bg-gradient-to-b from-[#9290FC] to-[#6A67EA] hover:from-[#6A67EA] hover:to-[#6A67EA] text-white py-2 px-4 rounded font-bold disabled:from-gray-400 disabled:to-gray-400 disabled:cursor-not-allowed` + ' ' + (props.full ? 'w-full' : 'w-auto')}
-            disabled={props.disabled}
-            >
-                {props.label}
-                {
-                    props.loader
-                    ?
-                    <i className="fa-solid fa-spinner fa-spin-pulse ml-2"></i>
-                    :
-                    ''
-                }
-            </button>
-        </>
-    )
+export default function Button({ btnType, label, loader, disabled, full }: any) {
+	return (
+		<>
+			<button
+				type={btnType ? btnType : "button"}
+				className={
+					`my-2 rounded bg-gradient-to-b from-[#9290FC] to-[#6A67EA] py-2 px-4 font-bold text-white hover:from-[#6A67EA] hover:to-[#6A67EA] disabled:cursor-not-allowed disabled:from-gray-400 disabled:to-gray-400` +
+					" " +
+					(full ? "w-full" : "w-auto")
+				}
+				disabled={disabled}
+			>
+				{label}
+				{loader ? <i className="fa-solid fa-spinner fa-spin-pulse ml-2"></i> : ""}
+			</button>
+		</>
+	);
 }
