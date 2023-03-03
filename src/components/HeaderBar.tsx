9f90551f2c7c48bc36eaf37883b8b4231734591a
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-03-03 13:37:31 +0300

    Added dark mode styles for header and job card

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,12 +1,12 @@
 export default function HeaderBar({ icon, title, handleBack }: any) {
 	return (
-		<div className="rounded-t-lg bg-white py-5 drop-shadow-md dark:bg-gray-800">
+		<div className="rounded-t-lg bg-white py-5 px-5 text-gray-800 drop-shadow-md dark:bg-gray-800 dark:text-white">
 			<div className="mx-auto w-full max-w-[1100px]">
-				<div className="flex flex-wrap items-center justify-start py-2 text-gray-800">
+				<div className="flex flex-wrap items-center justify-start py-2 ">
 					<button onClick={handleBack} className="justify-self-start">
 						<i className="fa-solid fa-arrow-left text-3xl"></i>
 					</button>
-					<div className="ml-20 text-2xl font-normal text-slate-600">
+					<div className="ml-20 text-2xl font-normal ">
 						<span>{title}</span>
 					</div>
 				</div>
