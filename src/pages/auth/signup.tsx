bb71372432d6eb2f93ae5e8296de7aec0b899895
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-03-03 13:36:32 +0300

    Refactored signin and signup for organization and candidate

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -20,84 +20,6 @@ const signUpInfoRules: Rules = {
 export default function SignUp() {
 	const router = useRouter();
 
-	const [formError, setFormError] = useState({
-		organizationName: null,
-		fullName: null,
-		email: null,
-		password: null,
-		passwordConfirm: ""
-	});
-	const updateSignUpInfo = (
-		prevState: {
-			email: string;
-			password: string;
-			passwordConfirm: string;
-			fullName: string;
-			organizationName: string;
-		},
-		event: { target: { id: string; value: any } }
-	) => {
-		const { id: key, value } = event.target;
-
-		const validation = new Validator(
-			{ [key]: value },
-			{ [key]: signUpInfoRules[key] },
-			{
-				min: "The password must be at least 8 characters long",
-				required: "This field is required",
-				regex: "Your password must be alphanumeric with at least one upper case letter"
-			}
-		);
-
-		if (!validation.passes()) setFormError((prev) => ({ ...prev, [key]: validation.errors.first(key) }));
-		else setFormError((prev) => ({ ...prev, [key]: null }));
-
-		return { ...prevState, [key]: value };
-	};
-
-	const [signUpInfo, dispatch] = useReducer(updateSignUpInfo, {
-		organizationName: "",
-		fullName: "",
-		email: "",
-		password: "",
-		passwordConfirm: ""
-	});
-
-	useEffect(() => {
-		if (
-			!formError.passwordConfirm &&
-			signUpInfo.passwordConfirm !== "" &&
-			signUpInfo.password !== signUpInfo.passwordConfirm
-		)
-			setFormError((prev) => ({ ...prev, passwordConfirm: "The confirmation password does not match" }));
-	}, [formError.passwordConfirm, signUpInfo.password, signUpInfo.passwordConfirm]);
-
-	async function handleSignUp(event: { preventDefault: () => void }) {
-		const form = new FormData();
-		Object.keys(signUpInfo).map((key) => form.append(key, signUpInfo[key as keyof typeof signUpInfo]));
-
-		event.preventDefault();
-		await axiosInstance.api
-			.post("/organization/registration/superadmin/", {
-				email: signUpInfo.email,
-				name: signUpInfo.fullName,
-				company_name: signUpInfo.organizationName,
-				password: signUpInfo.password,
-				password2: signUpInfo.passwordConfirm,
-				company_type: ""
-			})
-			.then((response) => {
-				console.log(response);
-				router.push("/auth/signin");
-				setTimeout(() => {
-					console.log("Send verification email");
-				}, 100);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}
-
 	return (
 		<>
 			<Head>
@@ -109,70 +31,28 @@ export default function SignUp() {
 					<div className="mb-4 text-center">
 						<Logo width={180} />
 					</div>
-					<form
-						className="min-h-[400px] rounded-large bg-white p-6 shadow-normal dark:bg-gray-800 md:py-8 md:px-12"
-						onSubmit={handleSignUp}
-					>
+					<div className="min-h-[400px] rounded-large bg-white p-6 shadow-normal dark:bg-gray-800 md:py-8 md:px-12">
 						<h1 className="mb-6 text-3xl font-bold">
 							Sign <span className="text-primary">Up</span>
 						</h1>
-						<FormField
-							fieldType="input"
-							inputType="text"
-							label="Organization Name"
-							id="organizationName"
-							handleChange={dispatch}
-							value={signUpInfo.organizationName}
-							error={formError.organizationName}
-							required
-						/>
-						<FormField
-							fieldType="input"
-							inputType="text"
-							label="Full Name"
-							id="fullName"
-							value={signUpInfo.fullName}
-							error={formError.fullName}
-							handleChange={dispatch}
-							icon={<i className="fa-regular fa-user"></i>}
-						/>
-						<FormField
-							fieldType="input"
-							inputType="email"
-							label="Email"
-							id="email"
-							value={signUpInfo.email}
-							handleChange={dispatch}
-							error={formError.email}
-							icon={<i className="fa-regular fa-envelope"></i>}
-							required
-						/>
-						<div className="-mx-3 flex flex-wrap">
-							<div className="mb-4 w-full px-3 md:max-w-[50%]">
-								<FormField
-									fieldType="input"
-									inputType="password"
-									label="Password"
-									id="password"
-									value={signUpInfo.password}
-									error={formError.password}
-									handleChange={dispatch}
-								/>
-							</div>
-							<div className="mb-4 w-full px-3 md:max-w-[50%]">
-								<FormField
-									fieldType="input"
-									inputType="password"
-									label="Confirm Password"
-									id="passwordConfirm"
-									value={signUpInfo.passwordConfirm}
-									error={formError.passwordConfirm}
-									handleChange={dispatch}
-								/>
-							</div>
-						</div>
+
 						<div className="mb-4">
-							<Button btnType="submit" label="Create Account" full={true} loader={false} disabled={false} />
+							<Button
+								btnType="button"
+								label="Candidate"
+								full={true}
+								loader={false}
+								disabled={false}
+								handleClick={() => router.push("/auth/candidate/signup")}
+							/>
+							<Button
+								btnType="button"
+								label="Organization"
+								full={true}
+								loader={false}
+								disabled={false}
+								handleClick={() => router.push("/auth/organization/signup")}
+							/>
 						</div>
 						<p className="text-center text-darkGray">
 							Already have an Account?{" "}
@@ -180,7 +60,7 @@ export default function SignUp() {
 								Sign In
 							</Link>
 						</p>
-					</form>
+					</div>
 				</div>
 			</main>
 		</>
