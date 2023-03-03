09cd706a5b82611fd586121980045395770a4c01
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-03-03 18:15:39 +0300

    Refactored styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,22 +6,19 @@ import Header from "@/components/header";
 import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  return (
-    <>
-      <ThemeProvider attribute="class">
-        <SessionProvider session={session}>
-          <Head>
-            <meta name="viewport" content="viewport-fit=cover" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-          <Header />
-          <Component {...pageProps} />
-        </SessionProvider>
-      </ThemeProvider>
-    </>
-  );
+	return (
+		<>
+			<ThemeProvider attribute="class">
+				<SessionProvider session={session}>
+					<Head>
+						<meta name="viewport" content="viewport-fit=cover" />
+						<meta name="viewport" content="width=device-width, initial-scale=1" />
+						<link rel="icon" href="/favicon.ico" />
+					</Head>
+					<Header />
+					<Component {...pageProps} />
+				</SessionProvider>
+			</ThemeProvider>
+		</>
+	);
 }
