9367cdd7b3768922e4e7a38e3b745c50fc9e8382
anmol-somhako <anmol.verma@somhako.com>
2023-03-10 14:54:28 +0530

    search job design done

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,12 +2,94 @@ import { useTheme } from "next-themes";
 import Logo from "@/components/logo";
 import { useRouter } from "next/router";
 import { signOut } from "next-auth/react";
+import { useCarrierId } from "@/utils/code";
+import Image from "next/image";
+import ThemeChange from "./ThemeChange"
+import { Popover } from '@headlessui/react'
+import googleIcon from '/public/images/social/google-icon.png'
+import Link from "next/link";
 
 export default function Header() {
 	const router = useRouter();
 	const { theme, setTheme } = useTheme();
 
-	if (router.asPath === "/organization") {
+	const cid = useCarrierId((state) => state.cid)
+	const addcid = useCarrierId((state) => state.addcid)
+
+	if (router.asPath === "/organization/"+ cid +"/carrierpage" || router.asPath === "/organization/"+ cid +"/search-jobs" || router.asPath === "/organization/"+ cid +"/dashboard") {
+		return(
+			<>
+				<header className="bg-white shadow-normal dark:bg-gray-800">
+					<div className="mx-auto flex w-full max-w-[1920px] items-center justify-between py-3 px-4 md:px-10 lg:px-14">
+						<div className="flex items-center">
+							<Image src={googleIcon} alt={'Somhako'} width={200} className="max-h-[40px] w-auto mr-8" />
+							<ul className="flex text-sm font-semibold text-darkGray">
+								<li className="mx-3">
+									<Link href={"/organization/"+ cid +"/search-jobs"} className={`inline-block px-2 py-[10px] border-b-2 hover:text-primary border-b-primary text-primary`}>
+										Search Jobs
+									</Link>
+								</li>
+								<li className="mx-3">
+									<Link href={"/organization/"+ cid +"/dashboard"} className={`inline-block px-2 py-[10px] border-b-2 hover:text-primary border-b-transparent`}>
+										Dashboard
+									</Link>
+								</li>
+							</ul>
+						</div>
+						<div className="flex items-center">
+							<ThemeChange />
+							<Popover className="relative mr-6">
+								<Popover.Button>
+									<button type="button" className="text-darkGray dark:text-white uppercase relative">
+										<i className="fa-regular fa-bell text-[20px]"></i>
+										<span className="flex items-center justify-center w-[20px] h-[20px] rounded-full bg-primary text-white text-[8px] absolute right-[-10px] top-[-7px]">99+</span>
+									</button>
+								</Popover.Button>
+								<Popover.Panel className="absolute right-0 z-10 bg-white dark:bg-gray-700 w-[280px] p-4 shadow-normal rounded">
+									<h4 className="font-bold mb-3 text-lg">Notifications</h4>
+									<ul className="list-disc px-4 text-sm text-darkGray dark:text-white font-semibold max-h-[300px] overflow-y-auto">
+										<li className="py-2">
+										You have applied For Job
+										</li>
+										<li className="py-2">
+										Your Profile has been Shortlisted for this Job
+										</li>
+										<li className="py-2">
+										Your Profile has been Rejected for this Job
+										</li>
+									</ul>
+								</Popover.Panel>
+							</Popover>
+							<Popover className="relative">
+								<Popover.Button>
+									<button
+										type="button"
+										className="h-[35px] w-[35px] rounded-full bg-darkGray text-white"
+									>
+										S
+									</button>
+								</Popover.Button>
+								<Popover.Panel className="absolute right-0 z-10 bg-white dark:bg-gray-700 w-[150px] shadow-normal rounded overflow-hidden">
+									<ul className="text-sm">
+										<li>
+											<Link href="#" className="w-full block py-2 font-bold py-1 px-4 hover:bg-gray-200">
+												<i className="fa-solid fa-gear mr-3"></i>
+												Settings
+											</Link>
+										</li>
+										<li>
+											<button type="button" className="text-left w-full block py-2 font-bold py-1 px-4 text-white bg-red-500 hover:bg-red-600"><i className="fa-solid fa-right-from-bracket mr-3"></i> Logout</button>
+										</li>
+									</ul>
+								</Popover.Panel>
+							</Popover>
+						</div>
+					</div>
+				</header>
+			</>
+		)
+	}
+	else if (router.asPath === "/organization") {
 		return (
 			<>
 				<header className="bg-white shadow-normal dark:bg-gray-800">
@@ -43,5 +125,6 @@ export default function Header() {
 			</>
 		);
 	}
+	
 	return <></>;
 }
---
+++ src/pages/organization/[carrierpage]/search-jobs.tsx
@@ -0,0 +1,87 @@
+import Button from "@/components/button";
+import FormField from "@/components/formfield";
+import { getProviders } from "next-auth/react";
+
+export default function SearchJobs() {
+    return (
+        <>
+            <main className="py-8">
+                <div className="container flex flex-wrap">
+                    <div className="w-[300px] h-[calc(100vh-120px)] bg-white dark:bg-gray-800 shadow-normal border border-slate-300 rounded-normal sticky top-0">
+                        <div className="border-b py-2 px-6">
+                            <div className="relative">
+                                <input
+                                    type={'search'}
+                                    id={'jobSearch'}
+                                    className={`w-full rounded  border-0 text-sm dark:bg-gray-700 pl-8 focus:ring-0`}
+                                    placeholder="Search for Jobs"
+                                />
+                                <span className="absolute left-0 top-[2px] text-xl">
+                                    <i className="fa-solid fa-magnifying-glass"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <div className="py-4 px-6">
+                            <h2 className="font-semibold mb-2">Filters</h2>
+                            <div className="py-2">
+                                <FormField 
+                                fieldType="select" 
+                                placeholder="Location"
+                                />
+                                <FormField 
+                                fieldType="select" 
+                                placeholder="Experience"
+                                />
+                                <FormField 
+                                fieldType="select" 
+                                placeholder="Job Type"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-[calc(100%-300px)] pl-8">
+                        <h3 className="mb-6 font-bold text-xl">170 Jobs</h3>
+                        <div className="flex flex-wrap mx-[-7px]">
+                            {Array(8).fill(
+                                <div className="w-full md:max-w-[50%] px-[7px] mb-[15px]">
+                                    <div className="h-full bg-white dark:bg-gray-800 rounded-[10px] shadow-normal p-5">
+                                        <h4 className="font-bold text-lg mb-3">Software Engineer</h4>
+                                        <ul className="mb-3 flex flex-wrap items-center text-[12px] text-darkGray dark:text-white font-semibold">
+                                            <li className="mr-8">
+                                                <i className="fa-solid fa-location-dot mr-2"></i>
+                                                Remote
+                                            </li>
+                                            <li className="mr-8">
+                                                <i className="fa-regular fa-clock mr-2"></i>
+                                                Full Time
+                                            </li>
+                                            <li className="mr-8">
+                                                <i className="fa-solid fa-dollar-sign mr-2"></i>
+                                                50-55k
+                                            </li>
+                                        </ul>
+                                        <div className="flex flex-wrap justify-between items-center">
+                                            <div className="mr-4">
+                                                <Button btnStyle="sm" label="View" loader={false} />
+                                            </div>
+                                            <p className="font-bold text-darkGray dark:text-white text-[12px]">29 min ago</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export async function getServerSideProps(context: any) {
+	const providers = await getProviders();
+	return {
+		props: {
+			providers
+		}
+	};
+}
