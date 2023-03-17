0d38fccb7bc433f9d6c888f37020813328b8fcaf
kidesleo <kidus.dev@gmail.com>
2023-03-17 17:02:46 +0300

    Added revoke endpoint

---
+++ src/pages/api/integrations/gcal/revoke.ts
@@ -0,0 +1,57 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { google } from "googleapis";
+import { axiosInstance } from "@/utils";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../auth/[...nextauth]";
+import { Integration } from "@/utils/index";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const session = await getServerSession(req, res, authOptions);
+
+	if (!session) return res.status(200).redirect("/");
+
+	const { unique_id } = await axiosInstance.api
+		.get("/organization/listorganizationprofile/", { headers: { authorization: "Bearer " + session.accessToken } })
+		.then((response) => response.data[0]);
+
+	const { integrations }: { integrations: Array<any> } = await axiosInstance.api
+		.get("/organization/integrations/calendar/" + unique_id + "/", {
+			headers: { authorization: "Bearer " + session?.accessToken }
+		})
+		.then((response) => response.data)
+		.catch((err) => {
+			console.log({ err: err.data });
+			return { data: { success: false } };
+		});
+
+	console.log({ integrations });
+	const googleCalendarIntegration = integrations.find(
+		(integration: { provider: string }) => integration.provider == "google"
+	);
+
+	console.log({ googleCalendarIntegration });
+
+	const expiry_date = Number(googleCalendarIntegration.expires_in) + Date.now();
+
+	Integration.googleCalendarOAuth2Client.setCredentials({
+		...googleCalendarIntegration,
+		expiry_date,
+		token_type: "Bearer"
+	});
+
+	await Integration.googleCalendarOAuth2Client.revokeCredentials();
+
+	const response = await axiosInstance.api
+		.post("/organization/delete_calendar_integration/" + googleCalendarIntegration.id + "/", {
+			headers: { authorization: "Bearer " + session.accessToken, "Content-Type": "application/json" }
+		})
+		.then((res) => res.data)
+		.catch((err) => {
+			console.log(err);
+			return { data: { success: false } };
+		});
+
+	res.json({ success: true, response });
+}
