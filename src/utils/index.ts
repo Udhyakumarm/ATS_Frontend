f917d2d87650d1d246e5aba35dcae4ac6dc1c558
kidesleo <kidus.dev@gmail.com>
2023-03-17 17:00:22 +0300

    Added google calendar integration client to Util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { google } from "googleapis";
 import jwt from "jsonwebtoken";
 
 export namespace JwtUtils {
@@ -60,4 +61,13 @@ export namespace axiosInstance {
 			"Content-Type": "application/json"
 		}
 	});
+}
+export namespace Integration {
+	export const googleCalendarOAuth2Client = new google.auth.OAuth2(
+		process.env.GOOGLE_INTEGRATION_CLIENT_ID,
+		process.env.GOOGLE_INTEGRATION_CLIENT_SECRET,
+		(process.env.NODE_ENV === "production"
+			? process.env.NEXT_PUBLIC_PROD_FRONTEND
+			: process.env.NEXT_PUBLIC_DEV_FRONTEND) + "/api/integrations/gcal"
+	);
 }
