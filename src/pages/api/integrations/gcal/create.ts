9d5edd0cd1c1a429f7313b56117c10514dca08ba
kidesleo <kidus.dev@gmail.com>
2023-03-17 17:01:54 +0300

    Renamed endpoint

---
+++ src/pages/api/integrations/gcal/create.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Integration } from "@/utils/index";
+
+const scopes = [
+	// "https://www.googleapis.com/auth/calendar",
+	// "https://www.googleapis.com/auth/calendar.events",
+	// "https://www.googleapis.com/auth/calendar.events.readonly",
+	// "https://www.googleapis.com/auth/calendar.readonly"
+	"https://www.googleapis.com/auth/calendar.app.created"
+];
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	const url = Integration.googleCalendarOAuth2Client.generateAuthUrl({
+		access_type: "offline",
+		scope: scopes
+	});
+
+	res.redirect(url);
+}
