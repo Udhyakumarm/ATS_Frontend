c358e990569499918cc368977b290f5e491fdae6
kidesleo <kidus.dev@gmail.com>
2023-03-23 10:27:15 +0300

    Refactored how we do auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,16 @@
 import "@/styles/globals.css";
 import Head from "next/head";
-import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Header from "@/components/Header";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, signIn, useSession } from "next-auth/react";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 import "nprogress/nprogress.css";
 import Router from "next/router";
 import NProgress from "nprogress";
 import { Toaster } from "react-hot-toast";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-	const router = useRouter();
+export default function App({ Component, pageProps: { session, ...pageProps } }: any) {
 	useEffect(() => {
 		const handleRouteStart = () => NProgress.start();
 		const handleRouteDone = () => NProgress.done();
@@ -23,13 +20,13 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 		Router.events.on("routeChangeError", handleRouteDone);
 
 		return () => {
-		// Make sure to remove the event handler on unmount!
-		Router.events.off("routeChangeStart", handleRouteStart);
-		Router.events.off("routeChangeComplete", handleRouteDone);
-		Router.events.off("routeChangeError", handleRouteDone);
+			// Make sure to remove the event handler on unmount!
+			Router.events.off("routeChangeStart", handleRouteStart);
+			Router.events.off("routeChangeComplete", handleRouteDone);
+			Router.events.off("routeChangeError", handleRouteDone);
 		};
 	}, []);
-  
+
 	return (
 		<>
 			<Toaster />
@@ -41,10 +38,31 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 						<link rel="icon" href="/favicon.ico" />
 					</Head>
 					<Header />
-					<Component {...pageProps} />
+					{Component.noAuth ? (
+						<Component {...pageProps} />
+					) : (
+						<Auth>
+							<Component {...pageProps} />
+						</Auth>
+					)}
 				</SessionProvider>
 			</ThemeProvider>
 			<Analytics />
 		</>
 	);
+}
+
+function Auth({ children }: any) {
+	const { data: session, status } = useSession();
+	const isUser = !!session?.user;
+	useEffect(() => {
+		if (status == "loading") return; // Do nothing while loading
+		if (!isUser) signIn(); // If not authenticated, force log in
+	}, [isUser, status]);
+
+	if (isUser) {
+		return children;
+	}
+
+	return <></>;
 }
