0115c524205bc1187c56685cc9b94671306c7cb5
kidesleo <kidus.dev@gmail.com>
2023-03-29 11:31:18 +0300

    Added integration types

--- types/global.d.ts
+++ types/global.d.ts
@@ -6,3 +6,20 @@ interface CalendarEvent {
 	start: Date;
 	end: Date;
 }
+
+interface Integration {
+	access_token: string;
+	refresh_token: string;
+	expires_in: number;
+	is_expired: boolean;
+	provider: string;
+	scope: string;
+}
+
+interface CalendarIntegration extends Integration {
+	calendar_id: string;
+}
+
+interface MailIntegration extends Integration {
+	labelId: string;
+}
