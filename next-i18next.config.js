59c1f532025e18e1ba92823f61618c04b12c1caa
anmolv1307 <anmolv1307@gmail.com>
2023-06-23 20:28:45 +0530

    config file changes

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
+
 module.exports = {
+  debug: true,
     i18n: {
       defaultLocale: 'en',
       locales: ['en', 'ja'],
+      localePath: path.resolve('./public/locales')
     },
 }
