6dcce122c233748c31142c9e68881195599fb9f2
Doshi Naman <naman.programmer@gmail.com>
2023-10-10 17:00:46 +0530

    10/10/23

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,11 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  debug: true,
-    i18n: {
-      defaultLocale: 'en',
-      locales: ['en', 'ja'],
-      localePath: path.resolve('./public/locales')
-    },
-}
+	debug: true,
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "ja"],
+		localeDetection: true,
+		localePath: path.resolve("./public/locales")
+	}
+};
