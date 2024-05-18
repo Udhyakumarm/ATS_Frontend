c275322cf9dec0860e2ea51d9b51315135f73087
Doshi Naman <naman.programmer@gmail.com>
2024-05-18 10:10:31 +0530

    18/5/24 Crwalling stop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -76,6 +76,7 @@ function App({ Component, pageProps: { session, ...pageProps } }: any) {
 					<ThemeProvider attribute="class">
 						<SessionProvider session={session}>
 							<Head>
+								<meta name="robots" content="noindex" />
 								<meta name="viewport" content="viewport-fit=cover" />
 								<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 								{/* <link rel="icon" href="/favicon.ico" /> */}
