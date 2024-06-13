0ff8bdc141265ffef3c8f3c9cade25f074a58f70
Doshi Naman <naman.programmer@gmail.com>
2024-06-13 10:38:39 +0530

    "atsapi2 -> atsapi3"

--- next.config.js
+++ next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
 			"localhost",
 			"127.0.0.1",
 			"atsapi.somhako.com",
-			"atsapi2.somhako.com",
+			"atsapi3.somhako.com",
 			"images.pexels.com",
 			"cdn-images-1.medium.com",
 			"images.unsplash.com"
